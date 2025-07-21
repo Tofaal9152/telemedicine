9d116d66b8ed525c4d796265058f3f05e10ca2ab
Tofaal9152 <tofaal91522@gmail.com>
2025-07-21 15:30:38 +0600

    add new server again test

--- client/src/context/Peer.tsx
+++ client/src/context/Peer.tsx
@@ -36,16 +36,29 @@ export const PeerProvider = ({ children }: { children: React.ReactNode }) => {
   const [peer, setPeer] = useState<RTCPeerConnection | null>(null);
 
   useEffect(() => {
+    // const pc = new RTCPeerConnection({
+    //   iceServers: [
+    //     {
+    //       urls: [
+    //         "stun:stun.l.google.com:19302",
+    //         "stun:global.stun.twilio.com:3478",
+    //       ],
+    //     },
+    //   ],
+    // });
     const pc = new RTCPeerConnection({
       iceServers: [
         {
-          urls: [
-            "stun:stun.l.google.com:19302",
-            "stun:global.stun.twilio.com:3478",
-          ],
+          urls: "stun:stun.l.google.com:19302", // Keep STUN
+        },
+        {
+          urls: "turn:openrelay.metered.ca:80", // Free TURN server
+          username: "openrelayproject",
+          credential: "openrelayproject",
         },
       ],
     });
+
     setPeer(pc);
   }, []);
 
